77a20509a4153254c4c284b1650107596c06cf1e
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2014-10-21 16:58:12 -0200

    adicionado direcoes baixo e cima

--- js/Heroi.js
+++ js/Heroi.js
@@ -1,5 +1,7 @@
 var DIRECAO_ESQUERDA = 1;
 var DIRECAO_DIREITA = 2;
+var DIRECAO_CIMA = 3;
+var DIRECAO_BAIXO = 4;
 
 function Heroi(context, teclado, animacao) {
 	this.context = context;
@@ -19,6 +21,12 @@ Heroi.prototype = {
 		} else if (this.teclado.pressionada(SETA_DIREITA) && this.x < this.context.canvas.width - 20) {
 			this.direcao = DIRECAO_DIREITA;
 			this.x += 10;
+		} else if (this.teclado.pressionada(SETA_CIMA) && this.y > 0) {
+			this.direcao = DIRECAO_CIMA;
+			this.y -= 10;
+		} else if (this.teclado.pressionada(SETA_BAIXO) && this.y < this.context.canvas.height - 50) {
+			this.direcao = DIRECAO_BAIXO;
+			this.y += 10;
 		}
 	},
 	desenhar: function () {
@@ -26,15 +34,24 @@ Heroi.prototype = {
 	},
 	atirar: function () {
 		var tiro = new Bola(this.context);
-		tiro.x = this.x + 10;
-		tiro.y = this.y + 10;
+		tiro.x = this.x + 20 / 2;
+		tiro.y = this.y + 50 / 2;
 		tiro.raio = 2;
 		tiro.cor = 'red';
 
-		if (this.direcao === DIRECAO_ESQUERDA) {
-			tiro.velocidadeX = -20;
-		} else {
-			tiro.velocidadeX = 20;
+		switch (this.direcao) {
+			case DIRECAO_ESQUERDA:
+				tiro.velocidadeX = -20;
+				break;
+			case DIRECAO_DIREITA:
+				tiro.velocidadeX = 20;
+				break;
+			case DIRECAO_CIMA:
+				tiro.velocidadeY = -20;
+				break;
+			case DIRECAO_BAIXO:
+				tiro.velocidadeY = 20;
+				break;
 		}
 
 		this.animacao.novoSprite(tiro);
--- js/Teclado.js
+++ js/Teclado.js
@@ -1,5 +1,7 @@
 var SETA_ESQUERDA = 37;
 var SETA_DIREITA = 39;
+var SETA_CIMA = 38;
+var SETA_BAIXO = 40;
 var ESPACO = 32;
 
 function Teclado(elemento) {
