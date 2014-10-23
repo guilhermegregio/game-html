9039fe3b1a36639671824b98fd9448ee7d9a05df
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2014-10-23 14:02:42 -0200

    Capitulo 6 - fundo parallax

---
+++ js/Fundo.js
@@ -0,0 +1,27 @@
+function Fundo(context, imagem) {
+	this.context = context;
+	this.imagem = imagem;
+	this.velocidade = 0;
+	this.posicaoEmenda = 0;
+}
+
+Fundo.prototype = {
+	atualizar: function () {
+		var img = this.imagem;
+
+		var posicaoY = this.posicaoEmenda - img.height;
+
+		this.context.drawImage(img, 0, posicaoY, img.width, img.height);
+
+		posicaoY = this.posicaoEmenda;
+
+		this.context.drawImage(img, 0, posicaoY, img.width, img.height);
+	},
+	desenhar: function () {
+		this.posicaoEmenda += this.velocidade;
+
+		if (this.posicaoEmenda > this.imagem.height) {
+			this.posicaoEmenda = 0;
+		}
+	}
+}
